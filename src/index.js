260ba36f71d5b02a4545930fe9f282985cc12739
JaydenDev <willsjayden2@gmail.com>
2022-04-02 09:04:01 -0500

    Exmpl

--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
-const { Extension, type, api } = require('clipcc-extension');
-
-class MyExtension extends Extension {
+const { api, type, extension } = require('clipcc-extension');
+class ExampleExtension extends Extension {
     onInit() {
         api.addCategory({
-            categoryId: 'jdev.mochablox.category',
-            messageId: 'jdev.mochablox.category',
-            color: '#66CCFF'
+            categoryId: 'clipteam.example.category',
+            messageId: 'clipteam.example.category.category',
+            color: '#339900'
         });
         api.addBlock({
-            opcode: 'jdev.mochablox.hello',
+            opcode: 'clipteam.example.return',
             type: type.BlockType.REPORTER,
-            messageId: 'jdev.mochablox.hello',
-            categoryId: 'jdev.mochablox.category',
-            function: () => 'Hello, ClipCC!'
+            messageId: 'clipteam.example.return.message',
+            categoryId: 'clipteam.example.category',
+            param: {
+                VALUE: {
+                    type: type.ParameterType.STRING,
+                    default: 'Hello World!'
+                }
+            },
+            function: args => this.ReturnValue(args.VALUE)
+        });
+        api.addBlock({
+            opcode: 'clipteam.example.helloworld',
+            type: type.BlockType.COMMAND,
+            messageId: 'clipteam.example.helloworld.message',
+            categoryId: 'clipteam.example.category',
+            function: args => this.HelloWorld()
         });
     }
+    onUninit() {
+        api.removeCategory('clipteam.example.category');
+    }
+    ReturnValue(VALUE) {
+        return VALUE;
+    }
+    HelloWorld() {
+        console.log("Hello World!");
+        alert("Hello World!");
+    }
 }
-
-module.exports = MyExtension;
+module.exports = ExampleExtension;
